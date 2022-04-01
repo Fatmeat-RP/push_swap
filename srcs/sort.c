9383723b6040efbaaff4bcf795805105e2f9fb46
fatmeat <pathofyggdrasil@gmail.com>
2022-04-01 17:24:16 +0200

    basic sort

--- srcs/sort.c
+++ srcs/sort.c
@@ -1,2 +1,18 @@
-if (params->stack_size == 3)
-pb()
+void	sort(t_stack stack_a, t_stack stack_b)
+{
+	while (!sorted(stack_a))
+	{
+		j = 0;
+		while(while j < stack_size)
+		{
+			int num = *(stack_a)->elem;
+			if ((num >> i) & 1)
+				ra();
+			else
+				pb();
+			j++;
+		}
+		while (!empty(stack_b))
+			pa();
+	}
+}
